bfed61c9ed578b0281d0c7f2e77db8f7369f75b2
Yehor <yeros05040504@gmail.com>
2023-08-03 22:12:19 +0300

    Improved: db requests for BuyRequests

--- car_website/car_website/Controllers/AdminController.cs
+++ car_website/car_website/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using car_website.Interfaces;
+using car_website.Models;
 using car_website.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
@@ -28,16 +29,35 @@ namespace car_website.Controllers
             if (HttpContext.Session.GetInt32("UserRole") != 1 && HttpContext.Session.GetInt32("UserRole") != 2)
                 return RedirectToAction("Index", "Home");
             var requests = await _buyRequestRepository.GetAll();
-
+            List<User> usersCache = new List<User>();
+            List<Car> carsCache = new List<Car>();
             List<BuyRequestViewModel> list = new List<BuyRequestViewModel>();
             foreach (var request in requests)
             {
-                var buyer = await _userRepository.GetByIdAsync(ObjectId.Parse(request.BuyerId));
-                var car = await _carRepository.GetByIdAsync(ObjectId.Parse(request.CarId));
-                var seller = await _userRepository.GetByIdAsync(ObjectId.Parse(car.SellerId));
+                var buyer = await GetUser(usersCache, request.BuyerId);
+                var car = await GetCar(carsCache, request.CarId);
+                var seller = await GetUser(usersCache, car.SellerId);
                 list.Add(new BuyRequestViewModel(car, buyer, seller));
             }
             return View(list);
+        }
+        private async Task<User> GetUser(List<User> users, string id)
+        {
+            ObjectId userId = ObjectId.Parse(id);
+            if (users.Count(el => el.Id == userId) > 0)
+                return users.Find(el => el.Id == userId);
+            var user = await _userRepository.GetByIdAsync(userId);
+            users.Add(user);
+            return user;
+        }
+        private async Task<Car> GetCar(List<Car> cars, string id)
+        {
+            ObjectId carId = ObjectId.Parse(id);
+            if (cars.Count(el => el.Id == carId) > 0)
+                return cars.Find(el => el.Id == carId);
+            var car = await _carRepository.GetByIdAsync(carId);
+            cars.Add(car);
+            return car;
         }
     }
 }
