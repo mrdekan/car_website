5ea2e43f9ba4fe0b6dd1452939fdef5484ab2662
Yehor <yeros05040504@gmail.com>
2023-08-13 19:39:09 +0300

    Added: similar cars

---
+++ car_website/car_website/ViewModels/LiteCarViewModel.cs
@@ -0,0 +1,24 @@
+﻿using car_website.Models;
+using car_website.Services;
+using System.Globalization;
+
+namespace car_website.ViewModels
+{
+    public class LiteCarViewModel
+    {
+        public LiteCarViewModel(Car car, CurrencyUpdater currencyUpdater)
+        {
+            Id = car.Id.ToString();
+            Info = $"{car.Brand} {car.Model} {car.Year}";
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            Price = string.Format("{0:n0}", car.Price).Replace(",", " ");
+            PriceUAH = string.Format("{0:n0}", currencyUpdater.ConvertToUAH(car.Price)).Replace(",", " ");
+            PhotoURL = car.PhotosURL[0];
+        }
+        public string Id { get; set; }
+        public string Info { get; set; }
+        public string PhotoURL { get; set; }
+        public string Price { get; set; }
+        public string PriceUAH { get; set; }
+    }
+}
---
+++ car_website/car_website/wwwroot/js/carDetailPage.js
@@ -0,0 +1,26 @@
+﻿const similarCarsBlock = document.getElementById('similar-cars');
+getSimilarCars();
+
+
+
+//#region Ajax requests
+function getSimilarCars() {
+    fetch(`/Car/FindSimilarCars/${similarCarsBlock.getAttribute('carId')}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data != null && data.success == true) {
+                similarCarsBlock.innerHTML = '';
+                data.cars.forEach(car => {
+                    similarCarsBlock.innerHTML += `<div class="similarCar">
+                        <a href="/Car/Detail/${car.id}">${car.info}</a>
+                        <img alt="photo" src="${car.photoURL}">
+                        <p>${car.price} $</p>
+                        <p>${car.priceUAH} грн</p>
+                    </div>`;
+                });
+            }
+            
+        })
+        .catch(error => console.error("An error occurred while retrieving data:", error));
+}
+//#endregion
