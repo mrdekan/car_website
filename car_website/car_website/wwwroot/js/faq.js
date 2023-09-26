21e0334be9435e5c3dc0184aa9336e74a0df59f1
Yehor <yeros05040504@gmail.com>
2023-09-26 20:36:04 +0300

    Improved: leasing page

---
+++ car_website/car_website/wwwroot/js/faq.js
@@ -0,0 +1,6 @@
+﻿const faqs = document.querySelectorAll('.faq_question-btn');
+faqs.forEach((el) => {
+    el.addEventListener('click', () => {
+        el.parentElement.classList.toggle('open');
+    });
+});
