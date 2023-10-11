60eb8734a6dcddbc88ceeac76df3734769208ea5
Yehor <yeros05040504@gmail.com>
2023-10-11 12:10:29 +0300

    Added: fields in car editing view model

--- car_website/car_website/ViewModels/CarEditingViewModel.cs
+++ car_website/car_website/ViewModels/CarEditingViewModel.cs
@@ -1,4 +1,5 @@
-﻿using car_website.Models;
+﻿using car_website.Data.Enum;
+using car_website.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace car_website.ViewModels
@@ -16,11 +17,24 @@ namespace car_website.ViewModels
         public CarDetailViewModel OldData { get; set; }
         public string? Brand { get; set; }
         public string? Model { get; set; }
+        [Range(1900, 3000, ErrorMessage = "Уведіть коректне значення")]
         public int? Year { get; set; }
         public int? Mileage { get; set; }
-        public float? EngineVolume { get; set; }
+        [MaxLength(3, ErrorMessage = "Занадто велике значення")]
+        public string? EngineVolume { get; set; }
         public CarFeatures? Features { get; set; }
         [MaxLength(500, ErrorMessage = "Не більше 500 символів")]
         public string? Description { get; set; }
+        public string? VideoURL { get; set; }
+        [Range(1, 100, ErrorMessage = "Оберіть значення")]
+        public Transmission CarTransmission { get; set; }
+        [Range(1, 100, ErrorMessage = "Оберіть значення")]
+        public TypeBody Body { get; set; }
+        [Range(1, 100, ErrorMessage = "Оберіть значення")]
+        public TypeFuel Fuel { get; set; }
+        [Range(1, 100, ErrorMessage = "Оберіть значення")]
+        public TypeDriveline Driveline { get; set; }
+        [Range(0, 10, ErrorMessage = "Оберіть значення")]
+        public Color CarColor { get; set; }
     }
 }
