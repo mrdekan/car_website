022c82d27f2031d1d7336d51e9e234d08dbfe8c7
Yehor <yeros05040504@gmail.com>
2023-11-24 20:30:45 +0200

    Improved: show pass button

---
+++ car_website/car_website/wwwroot/js/showPassword.js
@@ -0,0 +1,17 @@
+﻿import svgCodes from "./svgCodesConst.js";
+const show_pass_btns = document.getElementsByName('show-password');
+show_pass_btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let pass_field = document.getElementById(btn.getAttribute('target-id'));
+        e.preventDefault();
+        if (btn.hasAttribute('shown')) {
+            pass_field.setAttribute('type', 'password');
+            btn.removeAttribute('shown');
+            btn.firstElementChild.innerHTML = svgCodes.shown;
+            return;
+        }
+        pass_field.setAttribute('type', 'text');
+        btn.setAttribute('shown', '');
+        btn.firstElementChild.innerHTML = svgCodes.hidden;
+    });
+});
