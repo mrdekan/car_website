8086ce3bf35527a94d8a21e435e07da9e3dc228b
Yehor <yeros05040504@gmail.com>
2024-10-18 14:14:51 +0300

    Added: build script

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set "archiveName=publish.zip"
+
+set "scriptDir=%~dp0"
+
+cd "%scriptDir%car_website\car_website" || (
+    echo Error: Not found: %scriptDir%car_website\car_website
+    pause
+    exit /b 1
+)
+
+dotnet build -c Release
+
+cd "bin\Release\net7.0\publish" || (
+    echo Error: Not found: %scriptDir%car_website\car_website\bin\Release\net7.0\publish
+    pause
+    exit /b 1
+)
+
+powershell -Command "Compress-Archive -Path * -DestinationPath '%scriptDir%%archiveName%' -Force"
+
+echo The build has been successfully completed
+
+pause
